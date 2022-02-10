480dbba2d778bcddeaa29b280a20555af80cb0f7
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-10 20:12:05 +0900

    [TailWind] darkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  // content (어디에서 tailwind를 사용할 것인가)
-  // 페이지(혹은 components)에서 모든 폴더(**), 모든 파일(*)에 적용할 것이며, 확장자명은 js or jsx or ts or tsx
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
@@ -8,5 +6,6 @@ module.exports = {
   theme: {
     extend: {},
   },
+  darkMode: "class",
   plugins: [],
 };
